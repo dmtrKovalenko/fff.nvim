c233372d4018f7e4f8b86adefb38aab0cc2b188d
Marc Jakobi <marc.jakobi@tiko.energy>
2025-08-18 18:36:02 +0200

    feat: lazy-load indexing

--- plugin/fff.lua
+++ plugin/fff.lua
@@ -1,1 +1,23 @@
-require('fff.main')
+if vim.g.fff_loaded then
+  return
+end
+vim.g.fff_loaded = true
+
+-- Defer indexing until after UIEnter, so as to not block the UI.
+-- This is equivalent to lazy.nvim's VeryLazy, but works with all plugin managers.
+
+if vim.v.vim_did_enter == 1 then
+  require('fff.main')
+else
+  vim.api.nvim_create_autocmd('UIEnter', {
+    group = vim.api.nvim_create_augroup('fff.main', {}),
+    once = true,
+    nested = true,
+    callback = vim.schedule_wrap(function()
+    if vim.v.exiting ~= vim.NIL then
+       return
+    end
+    require('fff.main')
+    end),
+  })
+end
